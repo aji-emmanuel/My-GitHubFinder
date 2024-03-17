753ba26c9abf90f84e42028b970c8b3105fe9838
aji-emmanuel <ajiemilo@gmail.com>
2024-03-17 11:18:44 +0100

    Test number only condition

--- src/components/pages/Tests.js
+++ src/components/pages/Tests.js
@@ -2,19 +2,28 @@ import React, { Fragment } from 'react'
 
 function TestPage() {
 
+    const numsOnly = async (e) =>{
+        let charCode = (e.which) ? e.which : e.keyCode;
+        if(charCode < 48 || charCode > 57){
+            e.preventDefault();
+            return false;
+        }
+        return true;
+    }
 
     const onlyNumbers = async (e) => {
         const elInput = e.target;
         // Get start of selection (caret offset when typing)
-        const nSelStart = elInput.selectionStart;
+        //const nSelStart = elInput.selectionStart;
         // Get last typed character (modify for your own needs)
-        const sLastTyped = elInput.value.substr(nSelStart-1, 1);
-        if(!(['0','1','2','3','4','5','6'].includes(sLastTyped))){
-            e.preventDefault();
+        //const sLastTyped = elInput.value.substr(nSelStart-1, 1);
+        // if(!(['0','1','2','3','4','5','6'].includes(sLastTyped))){
+        //     e.preventDefault();
+            
+        // console.log('Last typed character:', sLastTyped);
             elInput.value = elInput.value?.replace(/[^0-9]/g,'')?.trim();
-            return false;
-        }
-        return true;
+            return numsOnly(e);
+        //}
     };
     
     return (
