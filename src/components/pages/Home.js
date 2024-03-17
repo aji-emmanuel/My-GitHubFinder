8b2d5dd062ec9056c52914da72a4de5ecbe965e8
aji-emmanuel <ajiemilo@gmail.com>
2024-03-17 08:41:05 +0100

    Add test page to home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,14 @@
 import React, {Fragment} from 'react'
 import Users from '../users/Users'
 import Search from '../users/Search'
+import TestPage from './Tests'
 
 function Home() {
     return (
        <Fragment>
             <Search />
             <Users />
+            <TestPage />
         </Fragment>
     )
 }
